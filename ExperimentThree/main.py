db21c0cb622db97459cf02d0dcdc9b0244359a95
xwwwb <xuwenbo@88.com>
2023-03-24 08:23:05 +0800

    更新部分第三章

--- ExperimentThree/main.py
+++ ExperimentThree/main.py
@@ -3,7 +3,8 @@ from rich.console import Console
 console = Console()
 
 oldPrint = print
-
+from random import shuffle
+from operator import add
 
 def MyPrint(*args):
     def func(desc=''):
@@ -51,11 +52,80 @@ def ContentTwo():
         pass
     oldPrint(max(range(len(data)), key=data.__getitem__))
 
+
 def ContentThree():
-    pass
+    data = [1, 2, 3, 4]
+    oldPrint(len(data))
+    oldPrint(sum(data))
+    data = (1, 2, 3)
+    oldPrint(len(data))
+    oldPrint(sum(data))
+    data = {1, 2, 3}
+    oldPrint(len(data))
+    oldPrint(sum(data))
+    data = 'Readability counts.'
+    oldPrint(len(data))
+    data = {97: 'a', 65: 'A', 48: '0'}
+    oldPrint(len(data))
+    oldPrint(sum(data))
+
+
+def ContentFour():
+    print = oldPrint
+    data = list(range(20))
+    shuffle(data)
+    print(data)
+    print(sorted(data))
+    print(sorted(data, key=str))
+    print(sorted(data, key=str, reverse=True))
+    data = list(range(20))
+    shuffle(data)
+    print(data)
+    reversedData = reversed(data)
+    print(reversedData)
+    print(list(reversedData))
+    print(tuple(reversedData))
+
+
+def ContentFive():
+    print = oldPrint
+    num = int(input("请输入一个大于2的自然数："))
+    if num % 2 == 1:
+        print("这是一个奇数")
+    else:
+        print("这是一个偶数")
+    lst = eval(input("请输入一个列表，包含若干大于2的自然数"))
+    print("列表中所有元素的和为：", sum(lst))
+    print(1, 2, 3, 4, 5)
+    print(1, 2, 3, 4, 5, sep=",")
+    print(3, 5, 7, end=' ')
+    print(9, 11, 13)
+
+
+def ContentSix():
+    range1 = range(10)
+    range2 = range(1, 11)
+    range3 = range(1, 11, 2)
+    range4 = range(20, 0, -2)
+    print(range1)
+    print(range2)
+    print(range3)
+    print(range4[2])
+    print(list(range1), list(range2), list(range3), list(range4))
+    for i in range(10):
+        print(i, end=" ")
+
+    data = zip('1234',[1,2,3,4,5,6])
+    print(data)
+    print(list(data))
+    data = zip('1234',[1,2,3,4,5,6])
+    print(tuple(data))
+    data = zip('1234',[1,2,3,4,5,6])
+    for i in data:
+        print(i)
 
 if __name__ == '__main__':
-    funcList = ['One', 'Two', 'Three']
+    funcList = ['One', 'Two', 'Three', 'Four', 'Five']
     for func in funcList:
         funcName = 'Content' + func
         console.print(f"执行函数：{funcName}", style="bold blue")
