f5f96177909b3768c7ca9065a4ac99657d2402fa
xwwwb <xuwenbo@88.com>
2023-06-24 10:01:19 +0800

    完成实验6

--- test.py
+++ test.py
@@ -1,38 +1,45 @@
-import random
-def test(num):
-    # 这里的num一定是四位数
-    a = num // 1000
-    b = num % 1000 // 100
-    c = num % 100 // 10
-    d = num % 10
-    list = [a, b, c, d] # 这是四个数字
-    count = 0
-    while True:
-        if count > 7:
-            return False
-        # 拿到最大数
-        list_1 = sorted(list, reverse=True)
-        num_1 = list_1[0] * 1000 + list_1[1] * 100 + list_1[2] * 10 + list_1[3]
-        # 拿到最小数
-        list_2 = sorted(list)
-        num_2 = list_2[0] * 1000 + list_2[1] * 100 + list_2[2] * 10 + list_2[3]
-        # 差值
-        num_3 = num_1 - num_2
-        if num_3 == 6174:
-            return True
-        else:
-            a = num_3 // 1000
-            b = num_3 % 1000 // 100
-            c = num_3 % 100 // 10
-            d = num_3 % 10
-            list = [a, b, c, d]
-            count += 1
+# import random
+# def test(num):
+#     # 这里的num一定是四位数
+#     a = num // 1000
+#     b = num % 1000 // 100
+#     c = num % 100 // 10
+#     d = num % 10
+#     list = [a, b, c, d] # 这是四个数字
+#     count = 0
+#     while True:
+#         if count > 7:
+#             return False
+#         # 拿到最大数
+#         list_1 = sorted(list, reverse=True)
+#         num_1 = list_1[0] * 1000 + list_1[1] * 100 + list_1[2] * 10 + list_1[3]
+#         # 拿到最小数
+#         list_2 = sorted(list)
+#         num_2 = list_2[0] * 1000 + list_2[1] * 100 + list_2[2] * 10 + list_2[3]
+#         # 差值
+#         num_3 = num_1 - num_2
+#         if num_3 == 6174:
+#             return True
+#         else:
+#             a = num_3 // 1000
+#             b = num_3 % 1000 // 100
+#             c = num_3 % 100 // 10
+#             d = num_3 % 10
+#             list = [a, b, c, d]
+#             count += 1
+#
+# # 生成20个四位数
+# testList = []
+# for i in range(20):
+#     num = random.randint(1000, 9999)
+#     testList.append(num)
+#
+# result = all(True == test(i) for i in testList)
+# print(result)
 
-# 生成20个四位数
-testList = []
-for i in range(20):
-    num = random.randint(1000, 9999)
-    testList.append(num)
 
-result = all(True == test(i) for i in testList)
-print(result)
+#  for line in fp是不会自动去除\n的
+with open("./demo.txt","r") as fp:
+    for line in fp:
+        line = line.strip()
+        print(line)
